8672ea48b3e5f6c265d168d89db956caec6eff9b
unknown <you@example.comdedorec2@my.lanecc.edu>
2018-10-03 19:50:52 -0700

    added up

--- PokeAScreen/PokeAScreen/Screen2.cs
+++ PokeAScreen/PokeAScreen/Screen2.cs
@@ -7,19 +7,39 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 
 namespace PokeAScreen
 {
     [Activity(Label = "Screen2")]
-    public class Screen2 : Activity
+    public class Screen2 : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            // create ui
+            var layout = new LinearLayout(this);
+            layout.Orientation = Orientation.Vertical;
 
-           
+
+            // enable 'up'
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+
+            
+        }
+
+
+        // up functionality
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            if (item.ItemId != Android.Resource.Id.Home)
+            {
+                return base.OnOptionsItemSelected(item);
+            }
+            Finish();
+            return true;
         }
     }
 }
